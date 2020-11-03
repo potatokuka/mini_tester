fbb14dec38f27aa4c131c091625ca5b2093ccb88
Tishj <t_b@live.nl>
2020-11-03 16:55:41 +0100

    updated test script

--- unit_tester.sh
+++ unit_tester.sh
@@ -15,68 +15,108 @@ WHITE="\033[1m\033[37m"
 # cp ../minishell .
 # chmod 755 minishell
 
+MSHELL_PATH=../codam_minishell/
 
-function get_test(){
-	if [ "$2" == "-e" ]; then
-		CAT=1
-	else
-		CAT=0
+get_test() {
+	CAT=0
+	for var in $@; do
+		if [ "$var" = "-e" ]; then
+			CAT=1
+		fi
+	done
+
+	if ! [ -f "${MSHELL_PATH}minishell" ]; then
+		printf "${RED}ERROR${RESET}: Minishell path doesn't contain 'minishell' executable\nMinishell path is currently set to: \"${MSHELL_PATH}\"\nChange the MSHELL_PATH variable in the script to set the path\n"
+		exit 1
 	fi
-	while IFS= read -r line; do
-		# echo "Text read from file : $line"
-		run_test $line
-		# printf " $YELLOW--- NEW CMD ---\n\n$RESET"
-	done < "$1"
+
+	for var in $@; do
+		if [ "${var::1}" = "-" ]; then
+			continue
+		fi
+		if [ ! -f "$var" ]; then
+			printf "File '$var' does not exist\n"
+			continue
+		fi
+		while IFS= read -r line; do
+			# echo "Text read from file : $line"
+			run_test $line
+			# printf " $YELLOW--- NEW CMD ---\n\n$RESET"
+		done < "$var"
+	done
 }
 
-function run_test(){
+run_test() {
 	# echo "inside run_test"
 #	# echo $line
-	TEST1=$(echo $line "; exit" | ../minishell 2>&-)
+	RESULT=$(echo $line "; exit" | ${MSHELL_PATH}minishell 2>&-)
 	# echo "----"
-	# echo $TEST1
+	# echo $RESULT
 	FLAG=0
-	ES_1=$?
-	TEST2=$(echo $line "; exit" | bash 2>&-)
-	ES_2=$?
-	if [ "$TEST1" == "$TEST2" ] && [ "$ES_1" == "$ES_2" ]; then
-		printf " $GREEN%s$RESET" "OK"
+	EXIT_MS=$?
+	EXPECTED=$(echo $line "; exit" | bash 2>&-)
+	EXIT_BASH=$?
+	if [ "$RESULT" = "$EXPECTED" ] && [ "$EXIT_MS" = "$EXIT_BASH" ]; then
+		printf " "
+		1>&2 printf "$GREEN"
+		printf "%s" "[OK]"
+		1>&2 printf "$RESET"
 	else
-		printf " $RED%s$RESET" "X"
+		printf " "
+		1>&2 printf "$RED"
+		printf "%s" "[KO]"
+		1>&2 printf "$RESET"
 	fi
 	echo " "$line
-	if [ "$TEST1" != "$TEST2" ]; then
-		echo
-		echo
-		FLAG=1
-		# printf $RED"Your output :\n%.20s\n$RED$TEST1\n%.20s$RESET\n" "----"
-		printf $RED"Your output :\n"
-		if [ $CAT=1 ]; then
-			echo $TEST1 | cat -e
-		else
-			echo $TEST1
-		fi
-		# printf $GREEN"Expected output :\n%.20s\n$GREEN$TEST2\n%.20s$RESET\n" "----"
-		printf $GREEN"Expected output :\n"
-		if [ $CAT=1 ]; then
-			echo $TEST2 | cat -e
-		else
-			echo $TEST2
-		fi
-	fi
-	if [ "$ES_1" != "$ES_2" ]; then
-		echo
+	if [ "$RESULT" != "$EXPECTED" ]; then
 		echo
 		FLAG=1
-		printf $RED"Your exit status : $RED$ES_1$RESET\n"
-		printf $GREEN"Expected exit status : $GREEN$ES_2$RESET\n"
+		# printf $RED"Your output :\n%.20s\n$RED$RESULT\n%.20s$RESET\n" "----"
+		1>&2 printf "$RED"
+		printf "Your output :\n"
+		if [ "$CAT" = 1 ]; then
+			echo $RESULT | cat -e
+		else
+			echo $RESULT
+		fi
+		# printf $GREEN"Expected output :\n%.20s\n$GREEN$EXPECTED\n%.20s$RESET\n" "----"
+		1>&2 printf "$GREEN"
+		printf "Bash output :\n"
+		if [ "$CAT" = 1 ]; then
+			echo $EXPECTED | cat -e
+		else
+			echo $EXPECTED
+		fi
 	fi
-	if [ $FLAG == 1 ]; then
+	if [ "$EXIT_MS" != "$EXIT_BASH" ]; then
 		echo
-		printf " $YELLOW--- NEW CMD ---\n\n$RESET"
+		FLAG=1
+		1>&2 printf "$RED"
+		printf "Your exit status : "
+		1>&2 printf "$RED"
+		printf "$EXIT_MS"
+		1>&2 printf "$RESET"
+		echo
+		1>&2 printf "$GREEN"
+		printf "Bash exit status : "
+		1>&2 printf "$GREEN"
+		printf "$EXIT_BASH"
+		1>&2 printf "$RESET"
+		echo
+	fi
+	if [ $FLAG -eq 1 ]; then
+		printf "\n "
+		1>&2 printf "$YELLOW"
+		printf "%s NEW CMD %s\n\n" "---" "---"
+		1>&2 printf "$RESET"
 		sleep 0.1
 	fi
 }
 
-get_test $1 $2
+if [ $# -eq 0 ]; then
+	printf "${RED}ERROR${RESET}: Please provide atleast one argument\nUsage: ./unit_tester.sh <TEST_FILE> [OPTIONS]\n"
+	exit 1
+fi
+
+get_test $@
 # rm file1 file2 file3 doethet newfile.txt newfile test1 test2 test3 test4 x1 x2 x3 y1 y2 ilovewords.txt hardesttest.txt
